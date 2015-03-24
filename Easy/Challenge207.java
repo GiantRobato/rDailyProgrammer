f1075eb493758b3c877e695df186550d6a82c10c
penguindustin <penguindustin@gmail.com>
2015-03-24 08:57:24 -0800

    fixed tabbing and added challenge link

--- Easy/Challenge207.java
+++ Easy/Challenge207.java
@@ -1,40 +1,40 @@
-	//-------------------------------------------------------------------------
-	// Answer to Challenge #207 [Easy] Bioinformatics 1: DNA Replication
-	//
-	// Full Question can be found here:
-	// http://www.reddit.com/r/dailyprogrammer/comments/2zyipu/20150323_challenge_207_easy_bioinformatics_1_dna/
-	//
-	// Input:
-	// A A T G C C T A T G G C
-	//
-	// Output:
-	// T T A C G G A T A C C G
-	//
-	// Console Output:
-	// A A T G C C T A T G G C
-	// T T A C G G A T A C C G
-	//
-	//-------------------------------------------------------------------------
+//-------------------------------------------------------------------------
+// Answer to Challenge #207 [Easy] Bioinformatics 1: DNA Replication
+//
+// Full Question can be found here:
+// http://www.reddit.com/r/dailyprogrammer/comments/2zyipu/20150323_challenge_207_easy_bioinformatics_1_dna/
+//
+// Input:
+// A A T G C C T A T G G C
+//
+// Output:
+// T T A C G G A T A C C G
+//
+// Console Output:
+// A A T G C C T A T G G C
+// T T A C G G A T A C C G
+//
+//-------------------------------------------------------------------------
 
-	import java.util.HashMap;
-	public class Challenge207 {
-	
-		public static void main(String[] args) {
-			
-			HashMap<Character, Character> hm = new HashMap<Character, Character>();
-			
-			hm.put((Character)'A', new Character('T'));
-			hm.put((Character)'T', new Character('A'));
-			hm.put((Character)'G', new Character('C'));		
-			hm.put((Character)'C', new Character('G'));
-			hm.put((Character)' ', new Character(' '));
-			
-			String input = "A A T G C C T A T G G C";
-			
-			System.out.println(input);
-			
-			for(int i = 0; i < input.length(); i++){
-				System.out.print(hm.get(input.charAt(i)));
-			}	
-		}
+import java.util.HashMap;
+public class Challenge207 {
+
+	public static void main(String[] args) {
+		
+		HashMap<Character, Character> hm = new HashMap<Character, Character>();
+		
+		hm.put((Character)'A', new Character('T'));
+		hm.put((Character)'T', new Character('A'));
+		hm.put((Character)'G', new Character('C'));		
+		hm.put((Character)'C', new Character('G'));
+		hm.put((Character)' ', new Character(' '));
+		
+		String input = "A A T G C C T A T G G C";
+		
+		System.out.println(input);
+		
+		for(int i = 0; i < input.length(); i++){
+			System.out.print(hm.get(input.charAt(i)));
+		}	
 	}
+}
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # rDailyProgrammer
 My Answers to the sub reddit r/dailyProgrammer
 
-##Questions I've answered by Difficulty
+##Questions by Difficulty
 
 ### EASY
 
-* Challenge 207 (description) Solution: [Java](https://github.com/penguindustin/rDailyProgrammer/blob/master/Easy/Challenge207.java)
+* [Challenge 207](http://www.reddit.com/r/dailyprogrammer/comments/2zyipu/20150323_challenge_207_easy_bioinformatics_1_dna/) (description) Solution(s): [Java](https://github.com/penguindustin/rDailyProgrammer/blob/master/Easy/Challenge207.java)
